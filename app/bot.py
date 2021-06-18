dc218ec793b9ecf430724b687648f34173a6ec75
= <=>
2021-06-18 20:29:21 +0000

    improved logging functionality

--- app/bot.py
+++ app/bot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import os
 from danbooru_api import danbooru_pic
+import log_bot
 
 
 # This loads environtmental values (download python-dotenv from pypi)
@@ -17,9 +18,13 @@ def main():
     @bot.event
     async def on_ready():
         print(f'{bot.user.name} has connected to Discord')
+        log_bot.start_log(bot.user.name)
 
     @bot.command(name='grab_pic', help='When the \'!!grab_pic [number of pics]\' is invoked, a random pic(s) from safebooru.donmai.us will be posted. ')
     async def grab_pic(ctx, limit_amount=1):
+        # Identify user
+        user = ctx.author.name
+        log_bot.log_request(limit_amount, user)
         if limit_amount > 5:
             await ctx.send("Please limit your request to 5 pics.")
         elif limit_amount <= 5:
@@ -28,10 +33,10 @@ def main():
                 #raw_grab is a list - slice at [posts] and then at ['file_url'] for picture URL, if it does not exist send 'preview_file_url'
                 try:
                     source = raw_grab[posts]['id']
-                    message = (f'{spacer} \n You can find this pic at this url: \'<https://safebooru.donmai.us/posts/{source}>\' \n')
+                    message = (f'{spacer} \n {user}, you can find this pic at this url: \'<https://safebooru.donmai.us/posts/{source}>\' \n')
                     pic = raw_grab[posts]['file_url']
                 except:
-                    message = 'Sorry Senpai, there was an issue with the request. 悪い、先輩。残念リクエストが失敗しました'
+                    message = (f'Sorry {user}-senpai, there was an issue with the request. 悪い、{user}-先輩。残念リクエストが失敗しました')
                     pic = None
                 await ctx.send(message)
                 await ctx.send(pic)
---
+++ app/log_bot.py
@@ -0,0 +1,28 @@
+import logging
+
+# see https://realpython.com/python-logging/
+
+# instaniate handler
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger_format = logging.Formatter('%(asctime)s-%(levelno)s-%(levelname)s-%(name)s-%(message)s')
+
+# Create logging handlers
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('app.log')
+
+# Set logging attributes
+console_handler.setFormatter(logger_format)
+file_handler.setFormatter(logger_format)
+
+# Add handlers to the 'logger'
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def start_log(name):
+    return logger.info(f"The {name} application has started.")
+
+def log_request(limit_amount, user):
+    return logger.info(f"{user} has requested {limit_amount} pics.")
+
+
